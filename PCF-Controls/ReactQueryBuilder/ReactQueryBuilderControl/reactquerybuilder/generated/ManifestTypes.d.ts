8d7de606aaa52d2ea39af52947a1b3da82b83e40
Gill, Nick <Nick.Gill@ais.com>
2026-01-13 15:35:37 -0500

    Update ReactQueryBuilder Control and Manifest Files
    - Bump version of @types/powerapps-component-framework to ^1.3.18 in package.json
    - Update pcf-scripts and pcf-start to ^1.51.1 in package.json
    - Increment control version to 0.0.36 in ControlManifest.Input.xml
    - Remove unused properties from ManifestTypes.d.ts
    - Delete outdated zip files: ReactQueryBuilder_Managed.zip and ReactQueryBuilder_Unmanaged.zip
    - Add new Solution.zip and Solution_managed.zip files
    - Update Solution.xml version from 1.0.0.1 to 1.0.0.2

--- PCF-Controls/ReactQueryBuilder/ReactQueryBuilderControl/reactquerybuilder/generated/ManifestTypes.d.ts
+++ PCF-Controls/ReactQueryBuilder/ReactQueryBuilderControl/reactquerybuilder/generated/ManifestTypes.d.ts
@@ -4,19 +4,13 @@
 
 // Define IInputs and IOutputs Type. They should match with ControlManifest.
 export interface IInputs {
-    sampleProperty: ComponentFramework.PropertyTypes.StringProperty;
     initialQuery: ComponentFramework.PropertyTypes.StringProperty;
-    fields: ComponentFramework.PropertyTypes.StringProperty;
     isreadonly: ComponentFramework.PropertyTypes.TwoOptionsProperty;
-    reset: ComponentFramework.PropertyTypes.TwoOptionsProperty;
     items: ComponentFramework.PropertyTypes.DataSet;
 }
 export interface IOutputs {
-    sampleProperty?: string;
     initialQuery?: string;
-    fields?: string;
     isreadonly?: boolean;
-    reset?: boolean;
     queryjson?: string;
     querysql?: string;
     formattedsqlquery?: string;
